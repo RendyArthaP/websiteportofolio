fddce40060f8df7223b5aa2bb9cea938b5705fa7
Rendy Artha <rendyartha312@gmail.com>
2021-02-12 22:21:27 +0700

    made a ornaments landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import imageProfile from './img/profile1.png'
+import Circle from './ornaments/Circle';
+import Dot from './ornaments/Dot';
 
 function App() {
   return (
-    <div className="flex flex-col text-center border h-screen items-center justify-center lg:flex-row-reverse lg:justify-around xl:px-10 2xl:px-20">
-      <div className="rounded-full w-64 h-64 bg-navbar absolute -top-36 -left-36 overflow-hidden"></div>
-      <div className="rounded-full w-32 h-32 bg-navbar absolute overflow-hidden top-28 -left-20"></div>
-      <img 
-        src={imageProfile} 
-        alt="profile1" 
-        className="w-56 border-profile border-2 rounded-full lg:w-64 mb-2 lg:mb-0"
-      />
-      <div className="flex flex-col items-center px-2 md:px-0 lg:items-start">
-        <h1 className="font-bold font-poppins text-2xl text-basic my-1 lg:text-4xl">What’s up, everyone?!</h1>
-        <h2 className="font-bold font-poppins text-xl text-basic my-1 lg:text-3xl">I hope you guys enjoy my personal page.</h2>
-        <h3 className="font-medium font-poppins text-lg text-basic my-1 lg:text-xl">I really appreciate it. Thank you! :D</h3>
-        <a href="/" className="bg-button w-28 h-11 rounded py-2 my-1 shadow-2xl">
-          <span className="font-poppins text-white text-base">
-            Click me!
-          </span>
-        </a>
+    <div className="flex h-screen items-center">      
+      <Circle />
+      <Dot />
+      <div className="flex flex-col items-center text-center w-full lg:flex-row-reverse lg:justify-around lg:px-14">
+        <div className="border-profile border-2 rounded-full w-56 h-56">
+          <img 
+            src={imageProfile} 
+            alt="profile1" 
+            className="rounded-full self-center -mt-4"
+          />
+        </div>
+        <div className="flex flex-col items-center px-2 md:px-0 lg:items-start">
+          <h1 className="font-bold font-poppins text-2xl text-basic my-1 lg:text-4xl">What’s up, everyone?!</h1>
+          <h2 className="font-bold font-poppins text-xl text-basic my-1 lg:text-3xl">I hope you guys enjoy my personal page.</h2>
+          <h3 className="font-medium font-poppins text-lg text-basic my-1 lg:text-xl">I really appreciate it. Thank you! :D</h3>
+          <a href="/" className="bg-button w-28 h-11 text-center rounded py-2 my-1 shadow-2xl">
+            <span className="font-poppins text-white text-base">
+              Click me!
+            </span>
+          </a>
+        </div>
       </div>
     </div>
   );
---
+++ src/ornaments/Dot.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Dot = () => {
+  return (
+    <div className="px-2 top-24 absolute right-32 lg:right-48 xl:right-64 2xl:right-80">
+      <div class="flex flex-row absolute">
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+      </div>
+      <div class="flex flex-row absolute my-4">
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+      </div>
+      <div class="flex flex-row absolute my-8">
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+      </div>
+      <div class="flex flex-row absolute my-12">
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+        <div className="rounded-full w-1 h-1 bg-content mx-1"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Dot
