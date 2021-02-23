68935cb36df855d78be7271c7557492e31fac2cc
Rendy Artha <rendyartha312@gmail.com>
2021-02-23 15:02:55 +0700

    create content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import imageProfile from '../img/profile1.png'
 
 const About = () => {
   return (
     <div>
       <Navbar />
-      <h1>About</h1>
+      <div>
+        <div className="border-profile border-2 rounded-full w-56 h-56">
+          <img 
+            src={imageProfile} 
+            alt="profile1" 
+            className="rounded-full self-center -mt-4"
+          />
+        </div>
+        <h1 className="font-bold font-poppins text-xl text-basic my-1 lg:text-3xl">Hello, my name is Rendy Artha Prawira</h1>
+        <h2 className="font-bold font-poppins text-md text-basic my-1 lg:text-xl">Video Editor to be Developer</h2>
+      </div>
     </div>
   )
 }
--- src/pages/Portofolio.js
+++ src/pages/Portofolio.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 const Portofolio = () => {
